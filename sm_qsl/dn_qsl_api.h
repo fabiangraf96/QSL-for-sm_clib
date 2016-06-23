edbe64072854bdd01d519e166074aceec486dae6
Jon-Håkon Bøe Røli <jhbr@datarespons.no>
2016-06-23 12:40:20 +0200

    Header cleanup

--- sm_qsl/dn_qsl_api.h
+++ sm_qsl/dn_qsl_api.h
@@ -1,11 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   dn_qsl_api.h
+ * Author: jhbr
+ *
+ * Created on 23. juni 2016, 12:36
+ */
+
+#ifndef DN_QSL_API_H
+#define DN_QSL_API_H
+
+#include "dn_common.h"
+
+//=========================== defined =========================================
+
 //=========================== typedef =========================================
 
+//=========================== variables =======================================
 
 //=========================== prototypes ======================================
-//==== API
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
 
 bool dn_qsl_init(void);
 bool dn_qsl_isConnected(void);
 bool dn_qsl_connect(uint16_t netID, uint8_t* joinKey, uint32_t service_ms);
 bool dn_qsl_send(uint8_t* payload, uint8_t payloadSize_B, uint8_t* destIP);
 uint8_t dn_qsl_read(uint8_t* payloadBuffer);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ sm_qsl/dn_watchdog.h
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   dn_watchdog.h
+ * Author: jhbr
+ *
+ * Created on 23. juni 2016, 12:36
+ */
+
+#ifndef DN_WATCHDOG_H
+#define DN_WATCHDOG_H
+
+#include "dn_common.h"
+
+//=========================== defined =========================================
+
+//=========================== typedef =========================================
+
+//=========================== variables =======================================
+
+//=========================== prototypes ======================================
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
