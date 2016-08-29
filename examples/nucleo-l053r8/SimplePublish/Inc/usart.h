b2a31028271aebbf7620c277e69b808a8f4b5175
Jon-Håkon Bøe Røli <jhbr@datarespons.no>
2016-08-29 12:30:42 +0200

    ex/nucleo/sp: _write implemented directly in usart.c with one call to HAL instead of bytewise calls; deleted syscalls.c

--- examples/nucleo-l053r8/SimplePublish/Inc/usart.h
+++ examples/nucleo-l053r8/SimplePublish/Inc/usart.h
@@ -58,13 +58,7 @@ void MX_USART1_UART_Init(void);
 void MX_USART2_UART_Init(void);
 
 /* USER CODE BEGIN Prototypes */
-#ifdef __GNUC__
-/* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
-   set to 'Yes') calls __io_putchar() */
-#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
-#else
-#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
-#endif /* __GNUC__ */
+
 
 void USART_SMIP_Interrupt(UART_HandleTypeDef *huart);
 /* USER CODE END Prototypes */
--- examples/nucleo-l053r8/SimplePublish/Src/usart.c
+++ examples/nucleo-l053r8/SimplePublish/Src/usart.c
@@ -188,19 +188,14 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle)
 } 
 
 /* USER CODE BEGIN 1 */
-/**
-  * @brief  Retargets the C library printf function to the USART.
-  * @param  None
-  * @retval None
-  */
-PUTCHAR_PROTOTYPE
-{
-  /* Place your implementation of fputc here */
-  /* e.g. write a character to the USART2 and Loop until the end of transmission */
-  HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
 
-  return ch;
+/* Implementation of _write for stdio to retarget printf to the ST-Link USART */
+int _write(int file, char *ptr, int len)
+{
+	HAL_UART_Transmit(&huart2, (uint8_t *)ptr, len, HAL_MAX_DELAY);
+	return len;
 }
+
 /* USER CODE END 1 */
 
 /**
