71d1e0b4e20430981011b274a807d3bbb6fcd9ff
Jon-Håkon Bøe Røli <jhbr@datarespons.no>
2016-08-31 14:47:00 +0200

    Added sleep comment to both Nucleo and Xplained examples

--- examples/samc21_xplained_pro/SimplePublish/src/dn_time.c
+++ examples/samc21_xplained_pro/SimplePublish/src/dn_time.c
@@ -39,7 +39,12 @@ uint32_t dn_time_ms(void)
 
 void dn_sleep_ms(uint32_t milliseconds)
 {
-	// TODO: Implement proper sleep?
+	/* A simple delay is used for simplicity in this example.
+	 * To save power, we could instead have initialized a timer to fire an
+	 * interrupt after the set number of milliseconds, followed by entering
+	 * a low-power sleep mode. Upon wake up, we would have to check that we
+	 * were indeed woken by said interrupt (and e.g. not an USART interrupt)
+	 * to decide if we should go back to sleep or not. */
 	delay_ms(milliseconds);
 }
 
